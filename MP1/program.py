96356eb39ab1b9bfe4073b4c9dc211ab3c71d86e
git_unspecified <git@unspecified.com>
2023-01-28 19:36:55 -0600

    Add depth

--- MP1/program.py
+++ MP1/program.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 from PIL import Image, ImageDraw
 from itertools import combinations
-np.set_printoptions(precision=2, suppress=True)
+np.set_printoptions(precision=2, suppress=True, threshold=90)
 
 lines = open(sys.argv[1], 'r').readlines()
 lines = map(lambda x: x.strip().replace('\t', ' '), lines)
@@ -34,16 +34,26 @@ def dda(a, b, d):
     p = a+o
     points = []
     while p[d] < b[d]:
+        # if p[d] == b[d]:
+            # points.append(p.copy())
         points.append(p.copy())
         p += s
     return points
 
 
 def draw(p):
+    do_draw = True
     pixel, color = tuple(map(round, p[:2])), tuple(map(round, p[4:]))
     depth = p[2]/p[3]
-    if not do_depth or (depth >= -1 and depth < depth_buffer[pixel]):
-        depth_buffer[pixel] = depth
+    if do_depth:
+        if depth >= -1 and depth <= depth_buffer[pixel]:
+            depth_buffer[pixel] = depth
+        else:
+            do_draw = False
+
+    if not (0 <= pixel[0] < width and 0 <= pixel[1] < height):
+        do_draw = False
+    if do_draw:
         img.putpixel(pixel, color)
 
 
@@ -73,15 +83,16 @@ for l in lines:
 
         # Do viewport transform
         vs = np.stack(map(viewport_transform, vs))
-        # print("view port transformed:\n", vs)
+        print("view port transformed:\n", vs)
 
         # DDA on y for each edge
+        # print("Pairs\n", np.stack(combinations(vs, 2)))
         edge_points = sum(map(lambda pair: dda(
             *pair, 1), combinations(vs, 2)), [])
         edge_points = np.array(
             sorted(edge_points, key=lambda x: x[1])).reshape(-1, 2, 8)
         # edge_points = np.unique(edge_points, axis=0)
-        # print("DDA edges", edge_points)
+        # print("DDA edges\n", edge_points)
 
         # DDA on x for each scan line
         vs = sum(map(lambda pair: dda(*pair, 0), edge_points), [])
@@ -90,6 +101,23 @@ for l in lines:
 
         # Draw pixel
         list(map(draw, vs))
+
+        # mx = min(vs, key = lambda x:x[0])
+        # my = min(vs, key = lambda x:x[1])
+        # print(f"{mx=},{my=}")
+        # mx[:2]-=10
+        # my[:2]-=10
+        # my[:2]=[10,10]
+        # draw(mx)
+        # draw(my)
+        # data = np.array([[24., 12., 1., 1., 255., 0., 0., 255.],
+                         # [6., 42.,  1., 1., 255., 0.,  0., 255.]])
+        # print(f"{data=}")
+        # line1 = dda(*data, 1)
+        # print("DDAed\n", np.array(line1))
+        # line2 = dda(line1[-1],line1[-1], 0)
+        # print("DDAed\n", np.array(line2))
+        # break
 
     print('--------------')
 
