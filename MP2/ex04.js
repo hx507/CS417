574ad2e08d43d7d7c00f3426600fdbab4d02574d
unknown <1198995013@qq.com>
2023-04-27 12:59:25 -0500

    Add more comments

--- MP2/ex04.js
+++ MP2/ex04.js
@@ -1,3 +1,7 @@
+/**
+* Given the source code of a vertex and fragment shader, compiles them,
+* and returns the linked program.
+*/
 function compileAndLinkGLSL(vs_source, fs_source) {
     let vs = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vs, vs_source)
@@ -25,6 +29,25 @@ function compileAndLinkGLSL(vs_source, fs_source) {
     }
 }
 
+/**
+ * Creates a Vertex Array Object and puts into it all of the data in the given
+ * JSON structure, which should have the following form:
+ *
+ * ````
+ * {"triangles": a list of of indices of vertices
+ * ,"attributes":
+ *  {name_of_vs_input_1: a list of 1-, 2-, 3-, or 4-vectors, one per vertex
+ *  ,name_of_vs_input_2: a list of 1-, 2-, 3-, or 4-vectors, one per vertex
+ *  }
+ * }
+ * ````
+ *
+ * @returns an object with four keys:
+ *  - mode = the 1st argument for gl.drawElements
+ *  - count = the 2nd argument for gl.drawElements
+ *  - type = the 3rd argument for gl.drawElements
+ *  - vao = the vertex array object for use with gl.bindVertexArray
+ */
 function setupGeomery(geom) {
     var triangleArray = gl.createVertexArray()
     gl.bindVertexArray(triangleArray)
@@ -56,20 +79,26 @@ function setupGeomery(geom) {
     }
 }
 
+// Maps the interval [0,1] continuously into the combined range of $$[-0.5,0.5] \cup [0.5,-0.5]$$, while maintaining the wrap around behavior
 function normalize(x) { //[0,1] -> [-0.5, 0.5] + [0.5->-0.5]
     return 2 * (x < 0.5 ? x : 1 - x) - .5;
 }
 
+// Maps real numbers to the ring modulo p, where speed controls the size of bands mapping to the same element.
 function cycle(x, p, speed) { // Map to a rational ring
     return ((x * speed) % p) / p;
 }
 
+// Clamps a number between the range [min,max]
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
+// Version of clamp() applying to all members in container
 function clamp_all(nums, min, max) {
     return nums.map((num) => clamp(num, min, max))
 }
 
+// Cancel the current requestAnimationFrame(), so nothing will be rendering and we can switch to new animation.
+// (May not be needed)
 function cancelAllAnimationFrames() {
     var id = window.requestAnimationFrame(function() {});
     while (id--) {
@@ -78,7 +107,7 @@ function cancelAllAnimationFrames() {
 }
 
 //https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
-// my own seedable RNG
+// my own seedable RNG, used so the vertex movement animation is deterministic
 function RNG(seed) {
     var m = 2 ** 35 - 31
     var a = 185852
@@ -88,6 +117,8 @@ function RNG(seed) {
     }
 }
 
+// Generate random numbers, *deterministically*, of a given size.
+// Using my own seedable RNG, used so the vertex movement animation is deterministic
 function randN(n, seed) { // create N randome numbers
     r = RNG(seed);
     let ret = [];
@@ -96,8 +127,11 @@ function randN(n, seed) { // create N randome numbers
     return ret;
 }
 
+// STUB: decide if move logic here to make things easier to read
 function cpuMoveVertices(vtx) {}
 
+// Draw one frame
+// We draw differently depending on which feature flags are enabled
 function draw(milliseconds) { // for the main dancing logo
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.useProgram(program)
@@ -112,7 +146,7 @@ function draw(milliseconds) { // for the main dancing logo
     if (window.do_mouse_response) { // use mouse vector as 2nd derivative
         let dt = 1 / 50;
         offset = window.curr_vel;
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 2; i++) {	// Move the logo depending on where the mouse is
             window.curr_vel[i] -= window.mouse_loc[i] * dt*5 * (i ? 1 : -1);
             window.curr_vel = clamp_all(window.curr_vel, -1, 1);
             window.curr_ofs[i] += window.curr_vel[i]*dt ;
@@ -126,11 +160,11 @@ function draw(milliseconds) { // for the main dancing logo
     // values that do not vary between vertexes or fragments are called "uniforms"
     gl.uniform1f(gl.getUniformLocation(program, 'seconds'), seconds)
     gl.uniform1f(gl.getUniformLocation(program, 'global_scale'), 1. / 4)
-    gl.uniform1f(gl.getUniformLocation(program, 'do_gpu_move'), 0)
+    gl.uniform1f(gl.getUniformLocation(program, 'do_gpu_move'), 0) // GPU move is passed as flags into the shader, and the shader will be responsible for moving it on GPU
 
     scale = clamp(scale, 0.1, 2);
     //offset = clamp_all(offset, -1, 1);
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'transform'), true, [
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'transform'), true, [ // Hand written translate matrix to move the model into the correct position
         scale, 0, 0, offset[0] * 1,
         0, scale, 0, offset[1] * 1,
         0, 0, scale, offset[2] * 1,
@@ -141,7 +175,7 @@ function draw(milliseconds) { // for the main dancing logo
         //if (scale <= 0.1) random_seed++; // rotate random offset seed for each animation iteration
         let pos = new Float32Array(window.geom_position_f32)
 
-        for (let i = 0; i < pos.length / 4; i++) {
+        for (let i = 0; i < pos.length / 4; i++) {	
             let nd_rand = 4; // only randomly offset 3 dimensions
             let ofs_vec = randN(nd_rand, i * random_seed);
             for (let j = i * 4; j < i * 4 + nd_rand; j++) { // randomly poke vertices
@@ -160,6 +194,7 @@ function draw(milliseconds) { // for the main dancing logo
     requestAnimationFrame(draw)
 }
 
+// Reset all the states, so a new animation can start fresh without being polluted by previous animation
 async function reset() { // reset states
     window.random_seed = 102; // my picked random seed at init
     window.do_cpu_move = false;
@@ -170,6 +205,9 @@ async function reset() { // reset states
     window.mouse_loc = [0, 0];
 }
 
+/**
+ * Compile, link, set up geometry
+ */
 async function setup(event) {
     reset();
 
@@ -197,6 +235,8 @@ async function setup(event) {
     requestAnimationFrame(draw)
 }
 
+// Reference function for drawing the warmp, not used
+// TODO: remove during clean up
 function drawWarmUp(milliseconds) {
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.useProgram(program)
@@ -210,6 +250,8 @@ function drawWarmUp(milliseconds) {
     gl.drawArrays(connection, 0, count)
     requestAnimationFrame(drawWarmUp)
 }
+// Reference function for drawing the warmp, not used
+// TODO: remove during clean up
 async function setupWarmUp(event) {
     reset();
     cancelAllAnimationFrames();
@@ -220,7 +262,9 @@ async function setupWarmUp(event) {
     requestAnimationFrame(drawWarmUp)
 }
 
-function drawPsy(milliseconds) { // for the main dancing logo
+// Called each frame during psydelics rendering
+// Renders a time-controlled 2d scaled sine function with changing color
+function drawPsy(milliseconds) { // for psydelics
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.useProgram(program)
 
@@ -242,6 +286,8 @@ function drawPsy(milliseconds) { // for the main dancing logo
     requestAnimationFrame(drawPsy)
 }
 
+// Called to initialize states used for psydelics rendering
+// Renders a time-controlled 2d scaled sine function with changing color
 async function setupPsy(event) {
     reset();
     cancelAllAnimationFrames();
